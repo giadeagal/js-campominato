9be991a154007d6e69e0db4c9a88719451f52459
Gianluca <gl.galassini@gmail.com>
2021-06-08 15:19:50 +0200

    chiedi numeri all'utente

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
 document.getElementById("printer").innerHTML = "<h1>Campo Minato</h1>";
-
+// +++++ FATTO +++++
 // Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+
+// +++++ DA FARE +++++
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-var bombs = [];
+var bombNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+var userNum = [];
 
-while (bombs.length < 16) {
+while (bombNum.length < 16) {
     var randomNum = getRandom(1, 100)
-    if (!bombs.includes(randomNum)) {
-        bombs.push(randomNum);
+    if (!bombNum.includes(randomNum)) {
+        bombNum.push(randomNum);
     }
 }
 
-console.log(bombs)
+while (userNum.length < 100 - 16) {
+    var insertNum = parseInt(prompt("Inserisci un numero da 1 a 100"));
+    if (!userNum.includes(insertNum)) {
+        userNum.push(insertNum);
+    }
+}
+
+console.log(bombNum);
+console.log(userNum);
 
 
 // Funzioni
